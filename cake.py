c8d640dd584840f2d2c9ad479595c4cd0b6afaa7
Ben North <ben@redfrontdoor.org>
2015-01-31 19:27:32 +0000

    main loop at 30 frames/sec

--- cake.py
+++ cake.py
@@ -3,3 +3,10 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((600, 600))
+clock = pygame.time.Clock()
+
+
+while True:
+    # Limit frame speed to 30 FPS
+    #
+    time_passed = clock.tick(30)
