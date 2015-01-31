7c60392ce535b3a91dab84fe1c886364b93d0a93
Ben North <ben@redfrontdoor.org>
2015-01-31 19:32:09 +0000

    draw icing palette down left edge of screen

--- cake.py
+++ cake.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
 
 
+icing_colours = ['red', 'green', 'blue', 'yellow']
+
 while True:
     # Limit frame speed to 30 FPS
     #
@@ -15,6 +17,13 @@ while True:
     screen.fill(pygame.Color('grey'))
 
     pygame.draw.circle(screen, pygame.Color('black'), (300, 300), 120, 2)
+
+    for i in range(len(icing_colours)):
+        pygame.draw.circle(screen,
+                           pygame.Color(icing_colours[i]),
+                           (60, 90 * (i + 1)),
+                           25,
+                           0)
 
     pygame.display.flip()
 
