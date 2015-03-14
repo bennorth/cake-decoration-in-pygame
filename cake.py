edba68a7a642f58e7cb47023446ed02a010ea318
Ben North <ben@redfrontdoor.org>
2015-03-14 18:56:59 +0000

    reformat only

--- cake.py
+++ cake.py
@@ -40,11 +40,7 @@ while True:
     pygame.draw.circle(screen, pygame.Color('black'), (300, 300), 120, 2)
 
     for i in range(len(icing_colours)):
-        pygame.draw.circle(screen,
-                           pygame.Color(icing_colours[i]),
-                           (60, 90 * (i + 1)),
-                           25,
-                           0)
+        pygame.draw.circle(screen, pygame.Color(icing_colours[i]), (60, 90 * (i + 1)), 25, 0)
 
     pygame.display.flip()
 
